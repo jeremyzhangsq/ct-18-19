e3dc9aa51e4215438f8a80a876cbc166b114fc31
e <s1885540@ed.ac.uk>
2018-10-04 14:44:14 +0100

    parsing pass: 75/75. Tested nested struct and multi dot.

--- tests/parse_test.c
+++ tests/parse_test.c
@@ -14,94 +14,116 @@
 //char sum [10);
 //void lyth aa;
 
-//// just main
-//main(){
+//// nest struct
 //
-//}
-int d;
-struct account {
-   int account_number;
-   char * first_name;
-   char * last_name;
-   int account;
-};
+//struct Info{
+//        char name[30];
+//        int age;
+//        struct address{
+//            char area_name[39];
+//            int house_no;
+//            char district[39];
+//        };
+//};
 
-struct nullstruct
+
+struct bstruct
 {
     int a;
     int b;
 }
+struct account {
+   int account_number;
+   char * first_name;
+   char * last_name;
+   struct bstruct bs;
+};
+
+struct account as;
+// multi dot
+int main(){
+    mul = as.b.c;
+    return 1;
+}
+
+//// just main
+//main(){
 //
-// function declare
-void func(){
-    print("func");
-}
-void func1(void a, void * b, int c, char d, struct account c, struct account * d){
-    print("func1");
-}
-
-void func1(void a,){
-    print("func1");
-}
-
-int a;
-int b;
-int sum;
-struct account * a;
-char sum [10];
-
-//// empty condition
-//int main(){
-//    while(){
-//        if(a){
-//                int a;
-//            }
-//    }
-//
-//    return 0;
 //}
-void fun1(int a, int b){
-    sum = 0;
-    a.account_number = 1;
+//int d;
 
-    (int) a;
-    c = (char*) b;
-//    d = (void[]) a;
-    a = (struct account) b;
-//    d = (account) a;
-//    d = (struct *) f;
-    d = (struct account*) ("a"<"d");
-    while (((5>3)||(a[10]!=1)))
-    {
-        void b;
-        struct account a;
-        a.account_number = 10;
-        x = x /10;
-        n = 0;
-        a();
-//        a = (x+ba+a)*6+;
-        if (x<1){
-            c = '\\';
-            d = (int*) 10;
-        }
-        // only else
-        else {
-            fun1();
-            while ((fun1()+2)/2>(int) 2)
-                i = i + 1;
-        }
 
-        c = '\\';
-        d = (int*) 10;
-
-    }
-    if (a){
-        c = '\\';
-        d = (int*) 10;
-    } else fun1();
-//    // exp as stmt
-//   func1()
-//   a < b
-//   sizeof(int)
-
-}
+////
+//// function declare
+//void func(){
+//    print("func");
+//}
+//void func1(void a, void * b, int c, char d, struct account c, struct account * d){
+//    print("func1");
+//}
+//
+//void func1(void a,){
+//    print("func1");
+//}
+//
+//int a;
+//int b;
+//int sum;
+//struct account * a;
+//char sum [10];
+//
+////// empty condition
+////int main(){
+////    while(){
+////        if(a){
+////                int a;
+////            }
+////    }
+////
+////    return 0;
+////}
+//void fun1(int a, int b){
+//    sum = 0;
+//    a.account_number = 1;
+//
+//    (int) a;
+//    c = (char*) b;
+////    d = (void[]) a;
+//    a = (struct account) b;
+////    d = (account) a;
+////    d = (struct *) f;
+//    d = (struct account*) ("a"<"d");
+//    while (((5>3)||(a[10]!=1)))
+//    {
+//        void b;
+//        struct account a;
+//        a.account_number = 10;
+//        x = x /10;
+//        n = 0;
+//        a();
+////        a = (x+ba+a)*6+;
+//        if (x<1){
+//            c = '\\';
+//            d = (int*) 10;
+//        }
+//        // only else
+//        else {
+//            fun1();
+//            while ((fun1()+2)/2>(int) 2)
+//                i = i + 1;
+//        }
+//
+//        c = '\\';
+//        d = (int*) 10;
+//
+//    }
+//    if (a){
+//        c = '\\';
+//        d = (int*) 10;
+//    } else fun1();
+////    // exp as stmt
+////   func1()
+////   a < b
+////   sizeof(int)
+//
+//}
