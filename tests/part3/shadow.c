8efd4044646f3fc5ba344bb190f5557de05f00d4
s1885540 <s1885540@ac.ed.uk>
2018-11-14 01:01:30 +0000

    Done: Int Char Shadowing

--- tests/part3/shadow.c
+++ tests/part3/shadow.c
@@ -2,17 +2,26 @@
 int main(){
     int x;
     x = 20;
+    char a;
+    a = 'o';
     int sum;
     sum = 0;
     int i;
     i = 0;
-    while(i < 5) {
-        int x;
-        x = i;
-        sum = sum + x;
-        i = i+1;
+    if (i<10){
+        char a;
+        a = 'i';
+        while(i < 5) {
+              int x;
+              x = i;
+              sum = sum + x;
+              i = i+1;
+        }
+        print_c(a);
     }
-
+    print_s((char*)"\n");
+    print_c(a);
+    print_s((char*)"\n");
     print_i(sum);
     print_s((char*)"\n");
     print_i(x);
