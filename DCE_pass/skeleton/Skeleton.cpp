43e7a5d08e2d99686a0b12d67bd04fa66c621ebb
s1885540 <s.zhang-79@sms.ed.ac.uk>
2018-11-21 18:23:11 +0000

    half done

--- DCE_pass/skeleton/Skeleton.cpp
+++ DCE_pass/skeleton/Skeleton.cpp
@@ -1,30 +1,32 @@
+#define DEBUG_TYPE "opCounter"
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include <vector>
+#include <map>
+//#include "llvm/IR/LegacyPassManager.h"
+//#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 using namespace llvm;
-
+using namespace std;
 namespace {
-  struct SkeletonPass : public FunctionPass {
+  struct CountOp : public FunctionPass {
+    std::map<std::string, int> opCounter;
     static char ID;
-    SkeletonPass() : FunctionPass(ID) {}
+    CountOp() : FunctionPass(ID) {}
+    virtual bool runOnFunction(Function &F){
+      errs() << "Function " <<F.getName() << "\n";
+      for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
+        for (BasicBlock::iterator i = bb->begin(), e = bb->end(); i != e; ++i) {
+          if (opCounter.find(i->getOpcodeName()) == opCounter.end()){
+            opCounter[i->getOpcodeName()] = 1;
+          }
+          else{
+            opCounter[i->getOpcodeName()] += 1;
+          }
+        }
+      }
 
-    virtual bool runOnFunction(Function &F) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
-      return false;
     }
   };
 }
 
-char SkeletonPass::ID = 0;
-
-// Automatically enable the pass.
-// http://adriansampson.net/blog/clangpass.html
-static void registerSkeletonPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  PM.add(new SkeletonPass());
-}
-static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerSkeletonPass);
